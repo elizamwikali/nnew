b6a3d54ddaeb7c272f4890458765cc370176ac66
elizamwikali <mwikalielizabeth6@gmail.com>
2018-06-01 21:57:30 +0300

    calender

---
+++ try.py
@@ -0,0 +1,13 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> list1 = [1,2,3,4,5,6]
+>>> print(list1)
+[1, 2, 3, 4, 5, 6]
+>>> type(list1)
+<class 'list'>
+>>> tuple1 = (1,3,4,3)
+>>> print(tuple1)
+(1, 3, 4, 3)
+>>> type(tuple1)
+<class 'tuple'>
+>>> dict1 = {'Name':'eliza','
